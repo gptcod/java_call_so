2fe1a1ec412c9834052b4761ca05f7e4f54aa103
liuyang <liuyang8@venustech.com.cn>
2017-10-10 10:03:06 +0800

    add test java program
    this program for split words by line and for test

---
+++ TextTransUtils.java
@@ -0,0 +1,26 @@
+﻿import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class TXTTransUtils {
+	public static void main(String[] args) {
+
+		/* 读取数据 */
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(new FileInputStream(new File("/home/text.txt")), "UTF-8"));
+			String lineTxt = null;
+			while ((lineTxt = br.readLine()) != null) {
+				
+				// do anything
+				System.out.println(name + 1);
+				
+			}
+			br.close();
+		} catch (Exception e) {
+			System.err.println("read errors :" + e);
+		}
+
+	}
+}
